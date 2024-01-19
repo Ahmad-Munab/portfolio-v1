45d0f97d67841be41e9c78bf180fc8b8bf3d7fbc
Ahmad Munab <ahmadmunab22@gmail.com>
2024-01-19 21:36:08 +0600

    added cv (basic old one) and gitignore, ready for deployment

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,28 +92,30 @@ export default function Intro() {
 
                 <a
                     className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                    href="/CV.pdf"
+                    href="/CV.png"
                     download
                 >
                     Download CV{" "}
                     <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
 
-                <div className="flex gap-2"><a
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://linkedin.com/in/ahmad-munab"
-                    target="_blank"
-                >
-                    <BsLinkedin />
-                </a>
+                <div className="flex gap-2">
+                    <a
+                        className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                        href="https://linkedin.com/in/ahmad-munab"
+                        target="_blank"
+                    >
+                        <BsLinkedin />
+                    </a>
 
-                <a
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://github.com/ahmad-munab"
-                    target="_blank"
-                >
-                    <FaGithubSquare />
-                </a></div>
+                    <a
+                        className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                        href="https://github.com/ahmad-munab"
+                        target="_blank"
+                    >
+                        <FaGithubSquare />
+                    </a>
+                </div>
             </motion.div>
         </section>
     );
